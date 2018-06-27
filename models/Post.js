5ea5956450a0d9f93a7c9a72c2551aaa48e86c0e
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 18:38:44 +0100

    Updated post model

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var PostSchema = mongoose.Schema({
     time: Date,
-    postBody: String
+    postBody: String,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
 module.exports = mongoose.model('Post', PostSchema);
