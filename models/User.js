c61286698ff2f3a142522bf03b70e63a88a37c59
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 21:53:39 +0100

    Changed posts type from ObjectId to Array

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ var UserSchema = mongoose.Schema({
    name: String,
    email: { type: String, unique: true},
    password: String,
-   posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+   posts: [{type: mongoose.Schema.Types.Array, ref: 'Post'}],
 });
 
 module.exports = mongoose.model('User', UserSchema);
