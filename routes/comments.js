3b3392f1ea2be5ecc7c3ca0ea946f32d3675cd13
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 19:19:33 +0100

    Modified routes

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,9 @@ var router = express.Router();
 var commentController = require('../controllers/CommentController');
 /* GET users listing. */
 router.post('/create', commentController.addComment);
-router.get('/post/:post', commentController.viewComments);
+router.get('/', commentController.getComments);
 router.get('/delete/:id', commentController.deleteComment);
+router.post('/update/:id', commentController.updateComment);
+router.get('/search', commentController.getCommentByParam);
 
 module.exports = router;
