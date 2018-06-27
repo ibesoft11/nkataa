bffbe62cfc4d988ffd8ed879c410350fed2e2208
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 19:01:35 +0100

    Updated routes for posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,16 +3,10 @@ var express = require('express');
 var router = express.Router();
 var postController = require('../controllers/PostController');
 /* GET users listing. */
-// router.post('/create', postController.createPost);
-// router.get('/', postController.viewPosts);
-// router.get('/delete/:id', postController.deletePost);
-// router.post('/edit', postController.editPost);
-// router.get('/user/:userId', postController.viewPostsByUser);
-
-router.post('/create', postController.createPost);
-router.get('/', userController.getUsers);
-router.get('/delete/:id', userController.deleteUser);
-router.get('/search', userController.getUserByParam);
-router.post('/update/:id', userController.updateUser);
+router.post('/create', postController.addPost);
+router.get('/', postController.getPosts);
+router.get('/delete/:id', postController.deletePost);
+router.get('/search', postController.getPostByParam);
+router.post('/update/:id', postController.updatePost);
 
 module.exports = router;
