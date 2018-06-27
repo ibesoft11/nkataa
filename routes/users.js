b629ad0863b431cd320d5ab95efd8dc1a96c4ec4
Ibesoft11 <ibesoft11@gmail.com>
2018-06-27 18:36:54 +0100

    Modified router for updating user records

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
+
 var router = express.Router();
 var userController = require('../controllers/UserController');
 /* GET users listing. */
 router.post('/create', userController.addUser);
 router.get('/', userController.getUsers);
 router.get('/delete/:id', userController.deleteUser);
+router.get('/search', userController.getUserByParam);
+router.post('/update/:id', userController.updateUser);
+
 module.exports = router;
